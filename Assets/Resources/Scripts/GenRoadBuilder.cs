2370f61247d2c87b7af506f6e2fdbafa50575a17
Ewen Auffret <auffret.ewen@gmail.com>
2017-04-19 17:42:35 +0200

    First commit, created map and blocks prefabs

---
+++ Assets/Resources/Scripts/GenRoadBuilder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GenRoadBuilder : MonoBehaviour {
+
+    private GameObject start;
+    private GameObject end;
+
+    public int maxRoadBlocks;
+    public GameObject map;
+
+	// Use this for initialization
+	void Start () {
+        start = GameObject.FindGameObjectWithTag("Start");
+        end = GameObject.FindGameObjectWithTag("End");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BuildRoad()
+    {
+
+        for (int i = 0; i < maxRoadBlocks; i++)
+        {
+
+        }
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/Map.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public int width;
+    public int length;
+
+    public GameObject[ , ] map;
+
+  
+    // Use this for initialization
+    void Start () {
+
+        createMap(width, length);
+        addStartBlock(13, 3);
+        addEndBlock(11, 13);
+        addCheckpointBlock(2, 6);
+        addCheckpointBlock(5, 11);
+
+    }
+    
+    private void createMap (int width, int length)
+    {
+        map = new GameObject[width, length];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                map[i, j] = (GameObject)Instantiate(Resources.Load(@"Prefabs/Node"), new Vector3(4.5f * i, 0, 4.5f * j), Quaternion.identity);
+            }
+        }
+    }
+    private void addStartBlock(int xPos, int yPos)
+    {
+        Destroy(map[xPos, yPos]);
+        map[xPos, yPos] = (GameObject)Instantiate(Resources.Load(@"Prefabs/Start"), new Vector3(4.5f * xPos, 0, 4.5f * yPos), Quaternion.identity);
+    }
+    private void addEndBlock(int xPos, int yPos)
+    {
+        Destroy(map[xPos, yPos]);
+        map[xPos, yPos] = (GameObject)Instantiate(Resources.Load(@"Prefabs/End"), new Vector3(4.5f * xPos, 0, 4.5f * yPos), Quaternion.identity);
+    }
+    private void addCheckpointBlock(int xPos, int yPos)
+    {
+        Destroy(map[xPos, yPos]);
+        map[xPos, yPos] = (GameObject)Instantiate(Resources.Load(@"Prefabs/Checkpoint"), new Vector3(4.5f * xPos, 0, 4.5f * yPos), Quaternion.identity);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
